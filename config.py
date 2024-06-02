032d35557bfddb3fd4b6d73b03eb74a627dad159
iCriptSnappie <rohitkumarjdmps99@gmail.com>
2024-06-02 12:23:01 +0530

    yy

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/iCriptSnappie/KK-VCMUSIC",
+    "https://github.com/iCriptSnappie/KK_MusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
