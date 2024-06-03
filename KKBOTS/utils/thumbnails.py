b01546fb46da90947072b2e357c2017b5b92f6e5
iCriptSnappie <rohitkumarjdmps99@gmail.com>
2024-06-03 19:46:46 +0530

    UPDATE

--- KKBOTS/utils/thumbnails.py
+++ KKBOTS/utils/thumbnails.py
@@ -73,7 +73,7 @@ async def get_thumb(videoid):
         draw = ImageDraw.Draw(background)
         arial = ImageFont.truetype("KKBOTS/assets/font2.ttf", 30)
         font = ImageFont.truetype("KKBOTS/assets/font.ttf", 30)
-        draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
+        draw.text((1100, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
             (55, 560),
             f"{channel} | {views[:23]}",
