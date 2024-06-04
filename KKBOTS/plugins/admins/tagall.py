bde332033d6fb63958c956f470c4261bc8563c3d
iCriptSnappie <rohitkumarjdmps99@gmail.com>
2024-06-04 11:21:21 +0530

    update

--- KKBOTS/plugins/admins/tagall.py
+++ KKBOTS/plugins/admins/tagall.py
@@ -263,7 +263,7 @@ async def mention_allvc(client, message):
 
 
 
-@app.on_message(filters.command(["cancel", "stop"]))
+@app.on_message(filters.command(["cancel", "stop", "stopvctag"]))
 async def cancel_spam(client, message):
     if not message.chat.id in spam_chats:
         return await message.reply("ᴄᴜʀʀᴇɴᴛʟʏ ɪ'ᴍ ɴᴏᴛ ᴛᴀɢɢɪɴɢ.")
