2a20e181149516e098ff916bcd61a4a710ee7cf2
Rohit Kumar <67968330+iCriptSnappie@users.noreply.github.com>
2024-06-22 19:48:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KernelKnight")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KernelKnightChats")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = False
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
 
 #Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST")
